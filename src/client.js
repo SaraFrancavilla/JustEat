733f0bb1959e6e075b9d59f8c67cdd94b7d86f63
AlicePython <scazzierialice@gmail.com>
2026-04-07 07:27:39 +0200

    replace deliveroo-js-client with deliveroo-js-sdk, unify sensing event

--- src/client.js
+++ src/client.js
@@ -1,12 +1,29 @@
-import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+import { DjsConnect } from "@unitn-asa/deliveroo-js-sdk/client";
 import { HOST, TOKEN } from "./config.js";
 
-const client = new DeliverooApi(HOST, TOKEN);
+const socket = await DjsConnect(HOST, TOKEN);
+
+const client = {
+  onConnect:        cb => socket.on('connect', cb),
+  onDisconnect:     cb => socket.on('disconnect', cb),
+  onYou:            cb => socket.on('you', cb),
+  onTile:           cb => socket.on('tile', cb),
+  onMap:            (...args) => socket.on('map', ...args),
+  onParcelsSensing: cb => socket.on('sensing', data => cb(data?.parcels ?? [])),
+  onAgentsSensing:  cb => socket.on('sensing', data => cb(data?.agents ?? [])),
+  onMsg:            cb => socket.on('msg', cb),
+  move:             dir => new Promise(resolve => socket.emit('move', dir, status => resolve(status))),
+  pickup:           () => new Promise(resolve => socket.emit('pickup', picked => resolve(picked))),
+  putdown:          (ids = null) => new Promise(resolve => socket.emit('putdown', ids, dropped => resolve(dropped))),
+  say:              (toId, msg) => new Promise(resolve => socket.emit('say', toId, msg, status => resolve(status))),
+  ask:              (toId, msg) => new Promise(resolve => socket.emit('ask', toId, msg, reply => resolve(reply))),
+  shout:            msg => new Promise(resolve => socket.emit('shout', msg, status => resolve(status))),
+};
 
 export const api = {
-    move: d => client.move ? client.move(d) : client.emitMove(d),
-    pickup: () => client.pickup ? client.pickup() : client.emitPickup(),
-    putdown: ids => client.putdown ? client.putdown(ids) : client.emitPutdown(ids)
+  move: (d) => client.move(d),
+  pickup: () => client.pickup(),
+  putdown: (ids) => client.putdown(ids),
 };
 
 export default client;
